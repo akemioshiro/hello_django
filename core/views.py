a290141fe3ef7a46487eb963856a70d51cfa0073
Patricia Akemi Oshiro <akemioshiro@hotmail.com>
2020-04-11 16:36:41 -0300

    nova rota para soma de valores

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('Hello {} de {} anos' .format(nome, idade))
+
+def soma(request, valor1, valor2):
+    return HttpResponse('A soma do valor {} com o valor {} é {}'.format(valor1, valor2, int(valor1)+int(valor2)))
